33f30c7fd984a5f293a8174224dc7aecdcd73725
Park Dong-Ha <luncliff@gmail.com>
2018-10-19 11:41:43 +0900

    +) Test Runner Script
     - Forwarding params to test program

--- Scripts/run-test.sh
+++ Scripts/run-test.sh
@@ -7,13 +7,17 @@ else
 fi
 pushd $DIR;
 
+# Forward leftover params to test program
+# https://stackoverflow.com/questions/1537673/how-do-i-forward-parameters-to-other-command-in-bash-script
+shift;
+# echo "$@";
+
 # Find all programs with given name
 # echo `ls | grep -i 'Problem*'`
-
 for program in `ls | grep -i 'Problem*'`
 do
 	echo $program;  # disply program's name
-    ./$program --reporter compact;     # execute the test program
+    ./$program $@;  # execute the test program
 done
 
 popd;
